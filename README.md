12c63f770240a57aa19ff74aa4ac35fe4234cef7
adimoon <ha_di@wp.pl>
2021-01-06 20:17:55 +0100

    Nonfunctional prototype

---
+++ README.md
@@ -0,0 +1,14 @@
+# el-lobo-client
+
+Klient czatu el-lobo
+
+[insert-logo-here]
+
+## Kompilacja
+
+> Qt
+
+## Uruchomienie
+
+> Qt
+
---
+++ lobo-client/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#define BUF_SIZE 128
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    tcpSocket(new QTcpSocket(this))
+{
+    connect(tcpSocket, &QIODevice::readyRead, this, &MainWindow::readData);
+    //typedef void (QAbstractSocket::*QAbstractSocketErrorSignal)(QAbstractSocket::SocketError);
+    //connect(tcpSocket, static_cast<QAbstractSocketErrorSignal>(&QAbstractSocket::error), this, &MainWindow::displayError);
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked(){
+
+    //qDebug() << ui->textBrowser->toPlainText();
+    //tcpSocket->connectToHost("127.0.0.1", 1234);
+    str += ui->lineEdit_2->text();
+    str += '\n';
+    ui->textBrowser->setText(str);
+    ui->lineEdit_2->clear();
+}
+
+void MainWindow::readData(){
+    //odbieranie danych
+    char buf[BUF_SIZE];
+    int n = tcpSocket->readLine(buf, BUF_SIZE);
+    buf[n] = 0;
+    qDebug() << buf;
+}
+
+void MainWindow::displayError(QAbstractSocket::SocketError socketError){
+
+    qDebug() << socketError;
+}
